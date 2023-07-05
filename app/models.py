f7426514df488cab930c1b6198f4580eca00eaf6
tylerkirumba <Tyler>
2023-07-05 16:20:34 +0300

    tables set

--- app/models.py
+++ app/models.py
@@ -3,23 +3,30 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
+    powers = db.relationship('HeroPower', backref='hero')
 
-class Power(db.Model):
-    __tablename__ = 'Power'
 
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'power'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    heroes = db.relationship('HeroPower', backref='power')
 
 
 class HeroPower(db.Model):
-    __tablename__ = 'HeroPower'
+    __tablename__ = 'hero_power'
 
-    power_id = db.Column(db,Integer, db.ForeignKey('power.id'))
-    hero_id  = db.Column(db.Integeer, db.Foregin('hero.id'))
-
-
-
-    
-# add any models you may need. 
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(10), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power = db.relationship('Power', backref='heroes')
+    hero = db.relationship('Hero', backref='powers')
